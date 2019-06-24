8dfd91a3a3c3c6167033aacaff289821e761708c
mort79f8 <mort79f8@edu.campusvejle.dk>
2019-06-24 09:54:39 +0200

    Er lidt begyndt på weather data

--- Sommerland.EntitiesTest/RideTest.cs
+++ Sommerland.EntitiesTest/RideTest.cs
@@ -24,6 +24,27 @@ namespace Sommerland.EntitiesTest
 
         }
 
-        public void 
+        [Fact]
+        public void DaysSinceLastShutdown_ReturnsCorrectDaysWithMultiBrokenReports()
+        {
+            //Arrange
+            Ride ride = new Ride();
+            DateTime reportTime = DateTime.Parse("2019-05-12 10:12:00");
+            Report report = new Report() { Status = Status.Broken, ReportTime = reportTime};
+            ride.Add(report);
+            DateTime reportTime2 = DateTime.Parse("2019-05-13 11:12:00");
+            Report report2 = new Report() { Status = Status.Broken, ReportTime = reportTime2 };
+            ride.Add(report2);
+            DateTime reportTime3 = DateTime.Parse("2019-05-22 01:01:00");
+            Report report3 = new Report() { Status = Status.Broken, ReportTime = reportTime3 };
+            ride.Add(report3);
+            int expectedDays = (DateTime.Now - reportTime3).Days;
+
+            //Act
+            int actualDays = ride.DaysSinceLastShutdown();
+
+            //Assert
+            Assert.Equal(actualDays, expectedDays);
+        }
     }
 }
---
+++ Sommerland.Service/Entities/Weather.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sommerland.Service.Entities
+{
+    class Weather
+    {
+        public string Main { get; set; }
+        public string Description { get; set; }
+    }
+}
